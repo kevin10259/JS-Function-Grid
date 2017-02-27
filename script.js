740157fa31fbf465a326ac4a18ae0379acae3a65
Kevin Moposita <kmoposita@aclanyc.com>
2017-02-27 13:21:38 +0000

    Add smiley face

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Write your function calls below this line
+setColor(0, 0, "red");
+setColor(9, 0, "red");
+setColor(0, 9, "red");
+setColor(9, 9, "red");
+
+setColor(4, 6, "black");
+setColor(4, 3, "black");
+setColor(6, 2, "black");
+setColor(7, 3, "black");
+setColor(7, 4, "black");
+setColor(7, 5, "black");
+setColor(7, 6, "black");
+setColor(6, 7, "black");
